8c9cdaa13b1c327234689f57f45a3cb1a07beabc
Jelle Kuipers <info@jellekuipers.com>
2025-03-12 15:30:09 +0100

    Checkbox

--- app/components/table/data-table-column-filter.tsx
+++ app/components/table/data-table-column-filter.tsx
@@ -1,12 +1,9 @@
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 import { Table } from "@tanstack/react-table";
-import { Label } from "radix-ui";
 
 import { Button } from "~/components/ui/button";
 import { Checkbox } from "~/components/ui/checkbox";
-import { Flex } from "~/components/ui/flex";
 import { Popover } from "~/components/ui/popover";
-import { Text } from "~/components/ui/text";
 
 interface DataTableColumnFilterProps<TData> {
   table: Table<TData>;
@@ -23,37 +20,23 @@ export function DataTableColumnFilter<TData>({
         </Button>
       </Popover.Trigger>
       <Popover.Content>
-        <Flex direction="column" gap="2">
+        <div className="flex flex-col gap-2">
           {table
             .getAllColumns()
             .filter((column) => column.getCanHide())
             .map((column) => {
               return (
-                <Flex
+                <Checkbox
                   key={column.id}
-                  align="center"
-                  style={{ cursor: "var(--cursor-button)" }}
+                  id={column.id}
+                  isSelected={column.getIsVisible()}
+                  onChange={(checked) => column.toggleVisibility(!!checked)}
                 >
-                  <Checkbox
-                    checked={column.getIsVisible()}
-                    id={column.id}
-                    onCheckedChange={(checked) =>
-                      column.toggleVisibility(!!checked)
-                    }
-                  />
-                  <Label.Root
-                    asChild
-                    htmlFor={column.id}
-                    style={{ paddingLeft: "var(--space-2)" }}
-                  >
-                    <Text as="label" size="2" weight="medium">
-                      {column.columnDef.header?.toString()}
-                    </Text>
-                  </Label.Root>
-                </Flex>
+                  {column.columnDef.header?.toString()}
+                </Checkbox>
               );
             })}
-        </Flex>
+        </div>
       </Popover.Content>
     </Popover.Root>
   );
--- app/components/ui/checkbox.tsx
+++ app/components/ui/checkbox.tsx
@@ -1,8 +1,24 @@
-import type { ComponentProps } from "react";
-import { Checkbox as RadixCheckbox } from "@radix-ui/themes";
+import { CheckIcon, MinusIcon } from "@radix-ui/react-icons";
+import { CheckboxProps, Checkbox as RACCheckbox } from "react-aria-components";
 
-export type CheckboxProps = ComponentProps<typeof RadixCheckbox>;
-
-export function Checkbox(props: CheckboxProps) {
-  return <RadixCheckbox {...props} />;
+export function Checkbox({ children, ...props }: CheckboxProps) {
+  return (
+    <RACCheckbox
+      className="flex gap-2 items-center text-sm font-medium"
+      {...props}
+    >
+      {({ isIndeterminate, isSelected }) => (
+        <>
+          <div className="size-5 shrink-0 rounded flex items-center justify-center border border-gray-300">
+            {isIndeterminate ? (
+              <MinusIcon aria-hidden />
+            ) : isSelected ? (
+              <CheckIcon aria-hidden />
+            ) : null}
+          </div>
+          {children}
+        </>
+      )}
+    </RACCheckbox>
+  );
 }
