c8f438d40060e1d1715575ff98c7177fe8374dcd
Jelle Kuipers <info@jellekuipers.com>
2025-03-12 21:30:57 +0100

    Scroll area

--- app/components/ui/scroll-area.tsx
+++
@@ -1,8 +0,0 @@
-import type { ComponentProps } from "react";
-import { ScrollArea as RadixScrollArea } from "@radix-ui/themes";
-
-export type ScrollAreaProps = ComponentProps<typeof RadixScrollArea>;
-
-export function ScrollArea(props: ScrollAreaProps) {
-  return <RadixScrollArea {...props} />;
-}
