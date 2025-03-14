4c9261f3f876a0f431549d370db4bbbacd810232
Jelle Kuipers <info@jellekuipers.com>
2025-03-14 23:01:00 +0100

    Table design

--- app/components/ui/table.tsx
+++ app/components/ui/table.tsx
@@ -6,45 +6,74 @@ import {
   TableBody as AriaTableBody,
   TableHeader as AriaTableHeader,
   CellProps,
+  CellRenderProps,
   ColumnProps,
+  ColumnRenderProps,
+  composeRenderProps,
   RowProps,
   TableBodyProps,
   TableHeaderProps,
   TableProps,
 } from "react-aria-components";
+import { tv } from "tailwind-variants";
+
+const tableCellStyles = tv({
+  base: "align-middle p-2 whitespace-nowrap first:pl-0 last:pr-0",
+  variants: {
+    isHeader: {
+      true: "font-bold",
+      false: "font-normal",
+    },
+  },
+});
 
 export function Table(props: TableProps) {
-  return <AriaTable className="w-full border-collapse" {...props} />;
+  return (
+    <div className="overflow-x-auto -mx-4 md:mx-0">
+      <AriaTable className="w-full" {...props} />
+    </div>
+  );
 }
 
 export function TableBody<T extends object>(props: TableBodyProps<T>) {
   return (
     <AriaTableBody
-      className="divide-y divide-gray-300 bg-white border-y border-gray-300"
+      className="align-middle divide-y divide-gray-300 border-y border-gray-300"
       {...props}
     />
   );
 }
 
-export function TableCell(props: CellProps) {
+export function TableCell({ className, ...props }: CellProps) {
   return (
     <AriaCell
-      className="first:pl-0 last:pr-0 px-4 py-3 whitespace-nowrap align-middle"
+      className={composeRenderProps(
+        className,
+        (className: string | undefined, renderProps: CellRenderProps) =>
+          tableCellStyles({ ...renderProps, className }),
+      )}
       {...props}
     />
   );
 }
 
-export function TableColumn(props: ColumnProps) {
+export function TableColumn({ className, ...props }: ColumnProps) {
   return (
-    <AriaColumn className="px-4 py-3 text-left whitespace-nowrap" {...props} />
+    <AriaColumn
+      className={composeRenderProps(
+        className,
+        (className: string | undefined, renderProps: ColumnRenderProps) =>
+          tableCellStyles({ ...renderProps, className, isHeader: true }),
+      )}
+      {...props}
+    />
   );
 }
 
 export function TableHeader<T extends object>(props: TableHeaderProps<T>) {
-  return <AriaTableHeader {...props} />;
+  return <AriaTableHeader className="align-middle" {...props} />;
 }
 
 export function TableRow<T extends object>(props: RowProps<T>) {
-  return <AriaRow {...props} />;
+  return <AriaRow className="align-middle" {...props} />;
 }
