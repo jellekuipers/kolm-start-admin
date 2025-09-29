5340691d4b7305214fe8e144decffb8a1d7e6e01
Jelle Kuipers <jellekuipers@Jelles-MacBook-Air.local>
2025-09-29 08:00:45 +0200

    1.2.3

--- src/routes/(authenticated)/route.tsx
+++ src/routes/(authenticated)/route.tsx
@@ -36,7 +36,7 @@ function LayoutComponent() {
           <Link className="rounded" to="/">
             <Logo size={32} />
           </Link>
-          <Code>v1.2.2</Code>
+          <Code>v1.2.3</Code>
         </div>
         <SessionUserDropdown />
       </header>
