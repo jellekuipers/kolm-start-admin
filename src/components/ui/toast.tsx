0315f00d621fcc43572b14c84b0b902a90fd33a3
Jelle Kuipers <jellekuipers@Jelles-MacBook-Air.local>
2025-10-22 11:51:33 +0200

    Toast

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -31,23 +31,26 @@ export const toastQueue = new AriaToastQueue<ToastContent>({
 const toastColors = {
   gray: twMerge(
     "bg-white text-gray-600 border border-gray-300",
-    "dark:bg-gray-700 dark:text-white",
+    "dark:bg-gray-700 dark:text-white dark:border dark:border-gray-700",
   ),
   green: twMerge(
-    "bg-green-50 text-green-600",
-    "dark:bg-green-700 dark:text-white",
+    "bg-green-50 text-green-600 border border-green-300",
+    "dark:bg-green-700 dark:text-white dark:border dark:border-green-700",
   ),
   indigo: twMerge(
-    "bg-indigo-50 text-indigo-700",
-    "dark:bg-indigo-600 dark:text-white",
+    "bg-indigo-50 text-indigo-700 border border-indigo-300",
+    "dark:bg-indigo-600 dark:text-white dark:border dark:border-indigo-700",
   ),
-  red: twMerge("bg-red-50 text-red-600", "dark:bg-red-700 dark:text-white"),
+  red: twMerge(
+    "bg-red-50 text-red-600 border border-red-300",
+    "dark:bg-red-700 dark:text-white dark:border dark:border-red-700",
+  ),
 } as const;
 
 export function Toast() {
   return (
     <AriaToastRegion
-      className="fixed bottom-4 right-4 flex flex-col-reverse gap-2"
+      className="fixed bottom-4 right-4 flex flex-col-reverse gap-2 max-w-80 w-full"
       queue={toastQueue}
     >
       {({ toast }) => {
@@ -58,7 +61,7 @@ export function Toast() {
         return (
           <AriaToast
             className={twMerge(
-              "flex justify-between items-start gap-4 rounded-lg p-4 text-sm w-full max-w-80",
+              "flex justify-between items-start gap-4 rounded-lg p-4 text-sm w-full",
               toastColors[color],
             )}
             style={{ viewTransitionName: toast.key }}
@@ -72,7 +75,7 @@ export function Toast() {
             </AriaToastContent>
             <AriaButton
               className={twMerge(
-                "flex size-8 items-center justify-center rounded",
+                "hover:opacity-75",
                 "outline-0 outline-offset-2 outline-indigo-700 focus-visible:outline-2",
               )}
               slot="close"
